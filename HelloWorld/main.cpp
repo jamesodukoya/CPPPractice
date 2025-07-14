729b6dd8230cdf2cd30ce4df3d714548b55070d1
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-07-13 23:58:22 -0500

    Introduction to iostream

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <iostream>  // for std::cout and std::cin
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Enter two numbers: ";
+
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Next number? ";
+
+    int y{};
+    std::cin >> y;
+
+    std::cout << "You entered " << x << " and " << y << '\n';
+
     return 0;
 }
