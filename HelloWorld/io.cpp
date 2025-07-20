78dc81ea1050a4a4157559ca1b50ee51e2cfb9e1
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-07-20 00:04:17 -0500

    Confirmed size of types

--- HelloWorld/io.cpp
+++ HelloWorld/io.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include "io.h"
+// #include <iostream>
+// #include "io.h"
 
-int readNumber()
-{
-    std::cout << "Enter your an integer: ";
+// int readNumber()
+// {
+//     std::cout << "Enter your an integer: ";
 
-    int num{};
-    std::cin >> num;
+//     int num{};
+//     std::cin >> num;
 
-    return num;
-}
+//     return num;
+// }
 
-void writeAnswer(int ans)
-{
-    std::cout << "Your answer is " << ans << "!\n";
-}
+// void writeAnswer(int ans)
+// {
+//     std::cout << "Your answer is " << ans << "!\n";
+// }
--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,24 @@
-#include "io.h"
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Please enter a number: ";
+    int x{};
+    std::cin >> x;
+
+    return x;
+}
+
+void writeAnswer(int x)
+{
+    std::cout << "The sum is: " << x << '\n';
+}
 
 int main()
 {
-    int firstNumber{readNumber()};
-    int secondNumber{readNumber()};
-
-    writeAnswer(firstNumber+secondNumber);
+    int x {readNumber()};
+    int y {readNumber()};
+    writeAnswer(x + y);
 
     return 0;
 }
