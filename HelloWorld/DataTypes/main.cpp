170c88c1e67a638921903211b8b2f7994536a19b
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-07-22 23:39:36 -0500

    Floating point precision

--- HelloWorld/DataTypes/main.cpp
+++ HelloWorld/DataTypes/main.cpp
@@ -1,14 +1,46 @@
-#include <iomanip> // for std::setw (which sets the width of the subsequent output)
+#include <iomanip> // for std::setw (which sets the width of the subsequent output). Generally for manipulating output. Also for std::setpreision()
 #include <iostream>
 #include <climits> // for CHAR_BIT
+#include <cstdint> // for fixed with integer std::intN_t
+#include <cstddef> // for typedef std::size_t
+#include <limits>
 
 int main()
 {
-    int x{};
-    std::cout << "x is " << sizeof(x) << " bytes\n";
+    std::cout << std::setprecision(17);
+
+    std::cout << 9.87654321 << '\n';
+    std::cout << 987.654321f << '\n';
+    std::cout << 987654.321f << '\n';
+    std::cout << 9876543.21f << '\n';
+    std::cout << 0.0000987654321f << '\n';
+
+    // std::cout << std::boolalpha; // print bool as true or false rather than 1 or 0
+    // std::cout << "float: " << std::numeric_limits<float>::is_iec559 << '\n';
+    // std::cout << "double: " << std::numeric_limits<double>::is_iec559 << '\n';
+    // std::cout << "long double: " << std::numeric_limits<long double>::is_iec559 << '\n';
+
+    // float a {1.0f};
+    // float b {a/3.0f};
+    // std::cout << b << '\n';
+
+    // int charge{119};
+    // charge = 5;
+    // std::cout << sizeof(charge) << " " << charge << '\n';
+
+    // std::cout << sizeof(std::size_t) << '\n';
 
     return 0;
 }
+
+// int main()
+// {
+// int x{5};
+// std::size_t s {sizeof(x)};
+// std::cout << s << " bytes\n";
+//
+// return 0;
+// }
 
 // int main()
 // {
