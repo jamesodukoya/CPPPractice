2372226a450429bc0b45bba8d126c426063b88f4
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-07-26 01:11:45 -0500

    Completed fundamental data types

--- HelloWorld/DataTypes/main.cpp
+++ HelloWorld/DataTypes/main.cpp
@@ -4,19 +4,109 @@
 #include <cstdint> // for fixed with integer std::intN_t
 #include <cstddef> // for typedef std::size_t
 #include <limits>
+#include <typeinfo>
+
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wmultichar"
+
+// If you want to ensure that a std::int8_t or std::uint8_t object is treated as an integer, you can convert the value to an integer using static_cast:
+
+double getNumber()
+{
+    std::cout << "Enter a double value: ";
+
+    double num{};
+    std::cin >> num;
+
+    return num;
+}
+
+char getOperator()
+{
+    std::cout << "Enter +, -, *, or /: ";
+
+    char oper{};
+    std::cin >> oper;
+
+    return oper;
+}
+
+void printResult(double firstNum, double secondNum, char sym)
+{
+    if (sym == '+')
+        std::cout << firstNum << " " << sym << " " << secondNum  << " is " << firstNum+secondNum << '\n';
+    else if (sym == '-')
+        std::cout << firstNum << " " << sym << " " << secondNum  << " is " << firstNum-secondNum << '\n';
+    else if (sym == '*')
+        std::cout << firstNum << " " << sym << " " << secondNum  << " is " << firstNum*secondNum << '\n';
+    else if (sym == '/')
+        std::cout << firstNum << " " << sym << " " << secondNum  << " is " << firstNum/secondNum << '\n';
+}
+
 
 int main()
 {
 
-    std::cout << "Enter a number 0 through 9 (inclusive): ";
-    int x{};
-    std::cin >> x;
-    if (x == 2 || x == 3 || x == 5 || x == 7)
-        std::cout << "The digit is prime!\n";
-    // else if (x<0)
-    //     std::cout << "The value is negative!\n";
-    else
-        std::cout << "The digit is not prime!\n";
+    double x{getNumber()};
+    double y{getNumber()};
+
+    char z {getOperator()};
+
+    printResult(x,y,z);
+
+    // std::cout << "Enter a single character: ";
+
+    // char ch{};
+    // std::cin >> ch;
+
+    // int i{ch};
+
+    // std::cout << "You entered \'" << ch << "\', which has ASCII code " << i << ".\n";
+
+
+    // int8_t newInt{};
+    // std::cin >> newInt; // I enter 35
+
+    // std::cout << newInt << '\n'; // Prints 51 (tutorial said it was meant to. But turns out it didn't): equivalent of '3'
+
+
+    // int s { -1 };
+    // std::cout << static_cast<unsigned int>(s) << '\n'; // prints 4294967295
+
+    // int t { -1 };
+    // std::cout << static_cast<uint16_t>(t) << '\n'; // prints 65535
+
+    // char ch{97};
+    // std::cout << "Character ch stores an integer value of " << static_cast<int>(ch) << '\n';
+
+    // double x{5.5};
+    // std::cout << "The value of x is " << static_cast<int>(x) << '\n';
+
+    // std::cout << typeid(x).name() << '\n';
+
+    // std::cout << "\\This is my first name.\\\n\t\t\t\"And this is my last name!\"\n\'\vWhich do you prefer?\a\'\n";
+
+    // std::cout << "Enter a character: ";
+    // char ch{};
+    // std::cin.get(ch);
+
+    // std::cout << "You entered this character: " << ch << '\n';
+
+    // std::cin.get(ch);
+    // std::cout << "You entered this character: " << ch << '\n';
+
+    // std::cin.get(ch);
+    // std::cout << "You entered this character: " << ch << '\n';
+
+    // std::cout << "Enter a number 0 through 9 (inclusive): ";
+    // char x{};
+    // std::cin >> x;
+    // if (x == 2 || x == 3 || x == 5 || x == 7)
+    //     std::cout << "The digit is prime!\n";
+    // // else if (x<0)
+    // //     std::cout << "The value is negative!\n";
+    // else
+    //     std::cout << "The digit is not prime!\n";
 
     // std::cout << std::boolalpha; // print bools as true or false
 
