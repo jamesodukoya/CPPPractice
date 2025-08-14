04e7e105e5562696a0279a564d1947441f0bce72
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-08-14 05:33:51 -0500

    Completed Bit Manipulation

--- HelloWorld/BitManipulation/main.cpp
+++ HelloWorld/BitManipulation/main.cpp
@@ -14,33 +14,63 @@ std::bitset<4> rotl(std::bitset<4> bits)
 
 int main()
 {
-    [[maybe_unused]] constexpr std::uint8_t mask0{0b0000'0001}; // represents bit 0
-    [[maybe_unused]] constexpr std::uint8_t mask1{0b0000'0010}; // represents bit 1
-    [[maybe_unused]] constexpr std::uint8_t mask2{0b0000'0100}; // represents bit 2
-    [[maybe_unused]] constexpr std::uint8_t mask3{0b0000'1000}; // represents bit 3
-    [[maybe_unused]] constexpr std::uint8_t mask4{0b0001'0000}; // represents bit 4
-    [[maybe_unused]] constexpr std::uint8_t mask5{0b0010'0000}; // represents bit 5
-    [[maybe_unused]] constexpr std::uint8_t mask6{0b0100'0000}; // represents bit 6
-    [[maybe_unused]] constexpr std::uint8_t mask7{0b1000'0000}; // represents bit 7
+
+    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
+    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
+    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
+    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
+    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
+
+    std::uint8_t myArticleFlags{ option_favorited };
+
+    myArticleFlags |= option_viewed;
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+
+    myArticleFlags &= static_cast<std::uint8_t>(~option_favorited);
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+
+    // std::cout << "Has the article been deleted? " << std::boolalpha << static_cast<bool>((myArticleFlags & option_deleted) >> 4) << '\n';
+
+
+    // [[maybe_unused]] constexpr std::uint8_t mask0{0b0000'0001}; // represents bit 0
+    // [[maybe_unused]] constexpr std::uint8_t mask1{0b0000'0010}; // represents bit 1
+    // [[maybe_unused]] constexpr std::uint8_t mask2{0b0000'0100}; // represents bit 2
+    // [[maybe_unused]] constexpr std::uint8_t mask3{0b0000'1000}; // represents bit 3
+    // [[maybe_unused]] constexpr std::uint8_t mask4{0b0001'0000}; // represents bit 4
+    // [[maybe_unused]] constexpr std::uint8_t mask5{0b0010'0000}; // represents bit 5
+    // [[maybe_unused]] constexpr std::uint8_t mask6{0b0100'0000}; // represents bit 6
+    // [[maybe_unused]] constexpr std::uint8_t mask7{0b1000'0000}; // represents bit 7
+
+    // std::uint8_t flags{ 0b0011'0101 }; // 8 bits in size means room for 8 flags
+
+
+    // // To toggle (flip) a bit state (from 0 to 1 or from 1 to 0), we use Bitwise XOR:
+    // std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
+    // flags ^= mask2; // flip bit 2
+    // std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
+    // flags ^= mask2; // flip bit 2
+    // std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
+
+
 
     // To reset (clear) a bit (to value 0), we use Bitwise AND and Bitwise NOT together
-    std::uint8_t flags{ 0b0011'0101 }; // 8 bits in size means room for 8 flags
+    // std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
 
-    std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
+    // std::cout << "bit 4 is " << (static_cast<bool>(flags & mask4) ? "on\n" : "off\n");
 
-    std::cout << "bit 4 is " << (static_cast<bool>(flags & mask4) ? "on\n" : "off\n");
+    // std::cout << "bit 5 is " << (static_cast<bool>(flags & mask5) ? "on\n" : "off\n");
 
-    std::cout << "bit 5 is " << (static_cast<bool>(flags & mask5) ? "on\n" : "off\n");
+    // flags &= static_cast<std::uint8_t>(~mask2); // turn off bit 2
 
-    flags &= static_cast<std::uint8_t>(~mask2); // turn off bit 2
+    // flags &= static_cast<std::uint8_t>(~(mask4|mask5)); // turns off bits 4 and 5 at the same time
 
-    flags &= static_cast<std::uint8_t>(~(mask4|mask5)); // turns off bits 4 and 5 at the same time
+    // std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
 
-    std::cout << "bit 2 is " << (static_cast<bool>(flags & mask2) ? "on\n" : "off\n");
+    // std::cout << "bit 4 is " << (static_cast<bool>(flags & mask4) ? "on\n" : "off\n");
 
-    std::cout << "bit 4 is " << (static_cast<bool>(flags & mask4) ? "on\n" : "off\n");
-
-    std::cout << "bit 5 is " << (static_cast<bool>(flags & mask5) ? "on\n" : "off\n");
+    // std::cout << "bit 5 is " << (static_cast<bool>(flags & mask5) ? "on\n" : "off\n");
 
 
     // Setting a bit
