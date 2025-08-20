949cde73802fb1cc35c744cca17873d41dda2955
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-08-20 05:41:20 -0500

    External linkage

--- HelloWorld/ScopeDurationLinkage/main.cpp
+++ HelloWorld/ScopeDurationLinkage/main.cpp
@@ -76,3 +76,10 @@ int main()
 
 // In modern C++, use of the static keyword for giving identifiers internal linkage is falling out of favor. Unnamed namespaces can give internal linkage to a wider range of identifiers (e.g. type identifiers), and they are better suited for giving many identifiers internal linkage.
 
+// To make a global variable external (and thus accessible by other files), we can use the extern keyword to do so:
+
+// int g_x { 2 }; // non-constant globals are external by default (no need to use extern)
+
+// extern const int g_y { 3 }; // const globals can be defined as extern, making them external
+// extern constexpr int g_z { 3 }; // constexpr globals can be defined as extern, making them external (but this is pretty useless, see the warning in the next section)
+
