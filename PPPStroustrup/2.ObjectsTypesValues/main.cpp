342c4fdd8e574504be52e5f8e0fb607f70e60e6d
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-08-29 07:00:32 -0500

    2.4 Operators and Operations

--- PPPStroustrup/2.ObjectsTypesValues/main.cpp
+++ PPPStroustrup/2.ObjectsTypesValues/main.cpp
@@ -1,4 +1,16 @@
+#include <string>
+#include <iostream>
+
 int main()
 {
+    std::cout << "Please enter your first name (followed by \"age\"): ";
+    std::string first_name{};
+    int age{};
+    std::cin >> first_name >> age;
+    std::cout << "Hello, " << first_name << ". Your age is " <<age << "!\n";
+
+
     return 0;
 }
+
+// By default, a string is initialized to ""
