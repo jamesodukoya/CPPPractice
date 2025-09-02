4f35f619137a0e34cc1b110f37efd2fca3905146
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-09-02 06:52:45 -0500

    Started object, types,, and values

---
+++ PPPStroustrup/2.ObjectsTypesValues/2.5.1DetectRepeatedWords/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter one or more sentences: ";
+    int number_of_words{0};
+    std::string previous{};
+    std::string current{};
+    while (std::cin >> current)
+    {
+        ++number_of_words;
+        if (current == previous)
+            std::cout << "Word number " << number_of_words << " repeated word: " << current << '\n';
+        previous = current;
+    }
+}
