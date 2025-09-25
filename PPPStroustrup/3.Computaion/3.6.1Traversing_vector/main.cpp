406f59021050123c41e77343e08ac74a5a579d09
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-09-25 07:07:28 -0500

    3.6.1Traversing_vector

---
+++ PPPStroustrup/3.Computaion/3.6.1Traversing_vector/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v = {0,1,2,3,4,5};
+    for (std::vector<int>::size_type i=0; i<v.size(); ++i)
+    {
+        std::cout << v[i] << '\n';
+    }
+}
--- PPPStroustrup/3.Computaion/main.cpp
+++ PPPStroustrup/3.Computaion/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 
+int square(int num)
+{
+    int square = 0;
+    for (int i = 0; i < num; ++i)
+    {
+        square += num;
+    }
+    return square;
+}
+
 int main()
 {
-    float small_pi = 3.14;
-    std::cout << double{small_pi} << '\n';
+
+    int num = 300;
+    std::cout << square(num) << '\n';
+
+    // for (char letter = 'a'; letter <= 'z'; ++letter) // in a for loop, the compiler doesn't care about the return value (post or pre-increment). It cares only about the side effect, which is that "letter" is being incremented or decremented
+    // {
+    //     std::cout << letter << '\t' << static_cast<int>(letter) << std::endl;
+    // }
+    // for (char i='A'; i<='Z'; ++i)
+    // {
+    //     std::cout << i << '\t' << static_cast<int>(i) << std::endl;
+    // }
+
+    // char letter = 'a';
+    // constexpr int num = static_cast<int>(letter);
+    // while (letter <= 'z')
+    // {
+    //     std::cout << letter << '\t' << static_cast<int>(letter) << std::endl;
+    //     ++letter;
+    // }
     return 0;
 }
