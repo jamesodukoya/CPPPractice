7110a7a3968c2c301800db64eae281d4b655ddf2
James Odukoya <odukoyaoluwadara@gmail.com>
2025-10-16 18:04:16 -0500

    Add Storage Classes

--- NeuralNine_QuickStart/StorageClasses/main.cpp
+++ NeuralNine_QuickStart/StorageClasses/main.cpp
@@ -2,8 +2,9 @@
 #include <typeinfo>
 
 int main(){
-    auto num = 10;
+    auto num = 10.9f;
     std::cout << (typeid(num).name()) << '\n';
+    std::cout << sizeof(num) << '\n';
 
     return 0;
 }
