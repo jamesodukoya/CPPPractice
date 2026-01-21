81d3177840d0b0ba5e8851340581021f6d2a8904
James Odukoya <93451762+jamesodukoya@users.noreply.github.com>
2026-01-21 21:50:51 +0000

    Test GitHub Codespaces

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
-// #include <iostream> // for std::cout and std::cin
+#include <iostream> // for std::cout and std::cin
 
-// // Receives user input of 3 umbers
-// // int main()
-// {
+// Receives user input of 3 umbers
+int main()
+{
     
-//     std::cout << "Enter an integer: ";
+    std::cout << "Enter an integer: ";
 
-//     int firstNum{};
-//     std::cin >> firstNum;
+    int firstNum{};
+    std::cin >> firstNum;
 
-//     std::cout << "Enter another integer: ";
+    std::cout << "Enter another integer: ";
 
-//     int secondNum{};
-//     std::cin >> secondNum;
+    int secondNum{};
+    std::cin >> secondNum;
 
-//     std::cout << firstNum << " + " << secondNum << " is: " << firstNum + secondNum << ".\n";
+    std::cout << firstNum << " + " << secondNum << " is: " << firstNum + secondNum << ".\n";
 
-//     std::cout << firstNum << " - " << secondNum << " is: " << firstNum - secondNum << ".\n";
+    std::cout << firstNum << " - " << secondNum << " is: " << firstNum - secondNum << ".\n";
 
-//     return 0;
-// }
+    return 0;
+}
